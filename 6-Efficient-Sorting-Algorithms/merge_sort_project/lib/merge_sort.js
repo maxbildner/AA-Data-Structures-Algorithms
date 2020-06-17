d3f508c05d7070cc6d95ff23667d1bd291cc4585
mbildner <mbildner@icloud.com>
2020-06-17 14:55:07 -0400

    add v1 naive solution for merge

---
+++ 6-Efficient-Sorting-Algorithms/merge_sort_project/lib/merge_sort.js
@@ -0,0 +1,76 @@
+// VERSION1- memory intensive, duplicates inputs, also uses shift and concat
+// [1, 5, 10, 15], [0, 2, 3, 7, 10]			=> [0, 1, 2, 3, 5, 7, 10, 10, 15]
+// [10, 13, 15, 25], []									=> [10, 13, 15, 25]
+// returns new sorted merged array. Note input arrays could be diff lengths
+function mergeV1(array1, array2) {
+	let mergedArr = [];
+	
+	// make copy of arrays
+	let leftArr = array1.slice();
+	let rightArr = array2.slice();
+
+	// loop until one of the copied arrays are empty
+	while (leftArr.length > 0 && rightArr.length > 0) {
+
+		// compare leftmost nums of both copied arrays, push smaller one to mergedArr
+		if (leftArr[0] < rightArr[0]) {
+
+			mergedArr.push(leftArr.shift());
+			
+			// else, push first num of rightArr to mergedArr (make sure to delete num from rightArr)
+		} else {
+			mergedArr.push(rightArr.shift());
+		}
+	}
+	
+	// if arr copies are diff lengths, then by this point one will be empty and other will not
+	// so merge them
+	return mergedArr.concat(leftArr, rightArr);
+}
+// console.log(mergeV1([1, 5, 10, 15], [0, 2, 3, 7, 10]));
+
+
+
+// VERSION2- 
+// [1, 5, 10, 15], [0, 2, 3, 7, 10]			=> [0, 1, 2, 3, 5, 7, 10, 10, 15]
+// [10, 13, 15, 25], []									=> [10, 13, 15, 25]
+// returns new sorted merged array. Note input arrays could be diff lengths
+function merge(array1, array2) {
+	let mergedArr = [];
+	
+	// make copy of arrays
+	let leftArr = array1.slice();
+	let rightArr = array2.slice();
+
+	// loop until one of the copied arrays are empty
+	while (leftArr.length > 0 && rightArr.length > 0) {
+
+		// compare leftmost nums of both copied arrays, push smaller one to mergedArr
+		if (leftArr[0] < rightArr[0]) {
+
+			mergedArr.push(leftArr.shift());
+			
+			// else, push first num of rightArr to mergedArr (make sure to delete num from rightArr)
+		} else {
+			mergedArr.push(rightArr.shift());
+		}
+	}
+	
+	// if arr copies are diff lengths, then by this point one will be empty and other will not
+	// so merge them
+	return mergedArr.concat(leftArr, rightArr);
+}
+console.log(merge([1, 5, 10, 15], [0, 2, 3, 7, 10]));
+
+
+
+function mergeSort(array) {
+
+}
+
+
+
+module.exports = {
+	merge,
+	mergeSort
+};
