5636388b2c60990584c9ae2cc93f2ab80ab9f561
mbildner <mbildner@icloud.com>
2020-06-17 15:17:24 -0400

    upload merge solution v2 without using concat (saves a little space)

--- 6-Efficient-Sorting-Algorithms/merge_sort_project/lib/merge_sort.js
+++ 6-Efficient-Sorting-Algorithms/merge_sort_project/lib/merge_sort.js
@@ -1,4 +1,6 @@
-// VERSION1- memory intensive, duplicates inputs, also uses shift and concat
+// VERSION1- memory intensive, duplicates inputs, also uses shift and concat, does not mutate inputs
+// Time: O(N)
+// Space: O(N)
 // [1, 5, 10, 15], [0, 2, 3, 7, 10]			=> [0, 1, 2, 3, 5, 7, 10, 10, 15]
 // [10, 13, 15, 25], []									=> [10, 13, 15, 25]
 // returns new sorted merged array. Note input arrays could be diff lengths
@@ -31,7 +33,8 @@ function mergeV1(array1, array2) {
 
 
 
-// VERSION2- 
+// VERSION2- AA Solution- avoids concatenation at end (saves a little memory)
+// Time: O(N)
 // [1, 5, 10, 15], [0, 2, 3, 7, 10]			=> [0, 1, 2, 3, 5, 7, 10, 10, 15]
 // [10, 13, 15, 25], []									=> [10, 13, 15, 25]
 // returns new sorted merged array. Note input arrays could be diff lengths
@@ -43,29 +46,36 @@ function merge(array1, array2) {
 	let rightArr = array2.slice();
 
 	// loop until one of the copied arrays are empty
-	while (leftArr.length > 0 && rightArr.length > 0) {
+	// while (leftArr.length > 0 && rightArr.length > 0) {
+	while (leftArr.length || rightArr.length) {																		// note* 0 considered falsey value
+		let leftNum = leftArr.length ? leftArr[0] : Infinity;												// Inifinity as default value incase one array is empty, without this we'll get infinite loop
+		let rightNum = rightArr.length ? rightArr[0] : Infinity;
+
+		let next;
 
 		// compare leftmost nums of both copied arrays, push smaller one to mergedArr
-		if (leftArr[0] < rightArr[0]) {
-
-			mergedArr.push(leftArr.shift());
+		if (leftNum < rightNum) {
+			next = leftArr.shift();
 			
 			// else, push first num of rightArr to mergedArr (make sure to delete num from rightArr)
 		} else {
-			mergedArr.push(rightArr.shift());
+			next = rightArr.shift();
 		}
+
+		// This line below also work instead of above if statement
+		// let next = (leftNum < rightNum) ? leftArr.shift() : rightArr.shift();
+
+		mergedArr.push(next);
 	}
 	
-	// if arr copies are diff lengths, then by this point one will be empty and other will not
-	// so merge them
-	return mergedArr.concat(leftArr, rightArr);
+	return mergedArr;
 }
-console.log(merge([1, 5, 10, 15], [0, 2, 3, 7, 10]));
+// console.log(merge([1, 5, 10, 15], [0, 2, 3, 7, 10]));
 
 
 
 function mergeSort(array) {
-
+	
 }
 
 
