d731f17ab4940ef9bc590e6d4e037f62430525cd
mbildner <mbildner@icloud.com>
2020-06-30 15:50:52 -0400

    upload solution to min_max_stack (similar to LC 155 min stack) using helper method

--- 9-Stacks-and-Queues/stack_queue_interview_problems/lib/2_min_max_stack.js
+++ 9-Stacks-and-Queues/stack_queue_interview_problems/lib/2_min_max_stack.js
@@ -1,5 +1,6 @@
 // ============================================================================
 // Interview Problem: Constant Time Stack Max
+// SIMILAR TO LEETCODE LC 155 Min Stack EASY
 // ============================================================================
 //
 // -------
@@ -59,57 +60,129 @@
 // minMaxStack.pop();
 // console.log(minMaxStack.min());   => null
 // console.log(minMaxStack.max());   => null
-//
-//
 // -----------
-// Let's code!
-// -----------
+
+
 class Node {
-    constructor(val) {
-        this.value = val;
-        this.next = null;
-    }
+	constructor(val) {
+		this.value = val;
+		this.next = null;
+	}
 }
 
+// VERSION 1- MY SOLUTION (uses helper method, easier to read, but similar to AA solution)
 // Refactor the regular Stack below into a MinMaxStack!
-class Stack {
-    constructor() {
-        this.top = null;
-        this.bottom = null;
-        this.length = 0;
-    }
+class MinMaxStack {
+	constructor() {
+		this.top = null;
+		this.bottom = null;
+		this.length = 0;		
+		this.minimums = [];													// stack array of nodes to track history of min vals
+		this.maximums = [];													// stack array of nodes to track history of max vals
+	}
 
-    push(val) {
-        const newNode = new Node(val);
-        if (!this.top) {
-            this.top = newNode;
-            this.bottom = newNode;
-        } else {
-            const temp = this.top;
-            this.top = newNode;
-            this.top.next = temp;
-        }
-        return ++this.length;
-    }
+	updateMinMax(node, calledFromPush) {
+		if (this.length === 0 && calledFromPush) {	// if stack empty and called from .push
+			this.minimums.push(node);									// push node to min and max arrays
+			this.maximums.push(node);
+			
+		} else if (calledFromPush) {								// stack not empty and called from .push
+			if (node.value > this.maximums[this.maximums.length - 1].value) this.maximums.push(node);
+			if (node.value < this.minimums[this.minimums.length - 1].value) this.minimums.push(node);
 
-    pop() {
-        if (!this.top) {
-            return null;
-        }
-        const temp = this.top;
-        if (this.top === this.bottom) {
-            this.bottom = null;
-        }
-        this.top = this.top.next;
-        this.length--;
-        return temp.value;
-    }
+		} else if (!calledFromPush) {								// stack not empty and called from .pop()
+			// if last node in minimums or maximums is the node we popped, 
+			// then pop last node in minimums/maximums
+			if (this.maximums[this.maximums.length - 1] === node) this.maximums.pop();
+			if (this.minimums[this.minimums.length - 1] === node) this.minimums.pop();
+		}
+	}
 
-    size() {
-        return this.length;
-    }
+	push(val) {
+		const newNode = new Node(val);
+		if (!this.top) {
+			this.top = newNode;
+			this.bottom = newNode;
+		} else {
+			const temp = this.top;
+			this.top = newNode;
+			this.top.next = temp;
+		}
+		
+		this.updateMinMax(newNode, true);									// check if min/max needs to be updated
+
+		return ++this.length;
+	}
+
+	pop() {
+		if (!this.top) {
+			return null;
+		}
+		const temp = this.top;
+		if (this.top === this.bottom) {
+			this.bottom = null;
+		}
+		this.top = this.top.next;
+		this.length--;
+
+		this.updateMinMax(temp, false);										// check if min/max needs to be updated
+
+		// return temp.value;
+		return temp;
+	}
+
+	size() {
+		return this.length;
+	}
+
+	min() {
+		if (this.top === null) return null;
+		return this.minimums[this.minimums.length - 1];		// return last node in array
+	}
+
+	max() {
+		if (this.top === null) return null;
+		return this.maximums[this.maximums.length - 1];		// return last node in array
+	}
 }
+
 
 // Forgetting something down here? 
 exports.Node = Node;
-exports.Stack = Stack;
+exports.MinMaxStack = MinMaxStack;
+
+
+// EX 1:
+// push 2
+// 	2  	min=2, max=2
+
+// push 2,  push 3
+// 	3
+// 	2  	min=2, max=3
+
+// push 2,  push 3,  push 2
+// 	2
+// 	3
+// 	2		min=2, max = 3
+
+// push 2,  push 3,  push 2,	 pop()
+// 	3
+// 	2		min=2, max = 3
+
+
+// EX 2:
+// push 3
+// 	3 	min=3, max=3
+
+// push 3,  push 2
+// 	2
+// 	3  	min=2, max=3
+
+// push 3,  push 2,  push 5
+// 	5
+// 	2
+// 	3		min=2, max=5
+
+// push 3,  push 2,  push 5,	 pop()
+// 	2
+// 	3		min=2, max=3
