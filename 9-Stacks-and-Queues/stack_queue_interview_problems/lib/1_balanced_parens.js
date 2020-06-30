ba384c243f5199183832cd26f1541eaa4f141aa6
mbildner <mbildner@icloud.com>
2020-06-30 12:53:47 -0400

    implement stack ADT to solve LC20 valid parentheses

--- 9-Stacks-and-Queues/stack_queue_interview_problems/lib/1_balanced_parens.js
+++ 9-Stacks-and-Queues/stack_queue_interview_problems/lib/1_balanced_parens.js
@@ -128,14 +128,66 @@ function balancedParensV1(str) {
 
 
 /******************************************************************************/
+// SOLUTION V2- AAA SOLUTION- Uses Stack ADT
+// TIME COMPLEXITY:   O(N),   N = string length
+// SPACE COMPLEXITY:  O(N),   bec. of stack array
+// ')('     => false
+// '{[]}'   => true      
+// '{[}]'   => false
+function balancedParens(str) {
+  // Here is an example of using a "Poor Man's Stack" to 
+  // manage limited-time interviews and get to the meat of the 
+  // interview problem quickly. 
+  //
+  // We are relying on arrays and on the developer following the
+  // honor system here. They must treat the array as if it is
+  // Stack.
+  //
+  // NEVER do this in an interview setting without confirming
+  // with the interviewer that they know you are purposefully
+  // taking a shortcut to avoid boring them with your Stack
+  // implementation. 
+  //
+  // You must look at your interviewer here, gauge their affect
+  // and the tonality of their voice, and make sure they appreciate 
+  // this as a gesture and that they will not dock points for 
+  // skipping something they originally wanted to test you on.
+  //
+  // Pulling this off is almost entirely a matter of how confidently
+  // you appear when you do it. If you exude so much confidence that
+  // the interivewer feels that implementing a proper OOP Stack class 
+  // from scratch is a waste of everyone's time in the room, 
+  // than you win.
 
+  let stack = [];                                                               // 1) create array lazy stack only holds valid LEFT brackets (push and pop methods only)
 
-console.log(balancedParens(')('));                    //=> false
-console.log(balancedParens('(((((((()'));             //=> false
-console.log(balancedParens('((([[[{{{ ]]]}}})))'));   //=> false   
-console.log(balancedParens('()'));                    //=> true
-console.log(balancedParens('(((((())))))'));          //=> true
-console.log(balancedParens('[{()}]'));                //=> true
+  const validPairs = {                                                          // 2) create hash table of valid bracket pairs (k/v = left parens/right parens)
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+
+  for (let i = 0; i < str.length; i++) {                                        // 3) loop through all chars in string
+    let char = str[i];
+
+    if (validPairs[char]) {                                                     // 4) if char is a valid LEFT bracket, push char to stack
+      stack.push(char);                                                         
+
+    } else if (char === ')' || char === ']' || char === '}') {                  // 5) else if char is valid RIGHT bracket
+
+      if (validPairs[stack.pop()] !== char) return false;                       // 6) invalid if the last char in stack NOT equal to char
+    }
+  }
+
+  return stack.length === 0;                                                    // 7) invalid if stack isn't empty (i.e. left/right brackets unbalanced)
+}
+
+// console.log(balancedParens(')('));                    //=> false
+// console.log(balancedParens('(((((((()'));             //=> false
+// console.log(balancedParens('((([[[{{{ ]]]}}})))'));   //=> false   
+// console.log(balancedParens('()'));                    //=> true
+// console.log(balancedParens('(((((())))))'));          //=> true
+// console.log(balancedParens('[{()}]'));                //=> true
 
 
 exports.balancedParens = balancedParens;
