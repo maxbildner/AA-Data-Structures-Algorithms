eb78d696c590690539f0726e0c3ca46d1a3c8ad3
mbildner <mbildner@icloud.com>
2020-07-01 16:08:36 -0400

    implement radixSort for negative and positive integers, update excel doc

--- 6-Efficient-Sorting-Algorithms/radix_sort_project/lib/radix_sort.js
+++ 6-Efficient-Sorting-Algorithms/radix_sort_project/lib/radix_sort.js
@@ -1,74 +1,37 @@
-// TIME COMPLEXITY: 		O(n * k),		n = array length, k = length of longest integer (maxDigits)
-// SPACE COMPLEXITY: 		O(n * k)
+// *****************************************************************************
+// SOLUTION V1- ONLY POSITIVE INTEGERS
+// TIME COMPLEXITY: 		O(N * L),		N = array length, L = length of longest integer (maxDigits)
+// SPACE COMPLEXITY: 		O(N + L)
 // - can only be used if we're sorting integers (or binary)
 // - radix = base of number system. Example humans use base 10 to count
 //   and there are 10 digits 0-9 (thus radix = 10). Ex2 binary has radix of 2
 // DOES NOT Mutate the original array
-// SOLUTION FOR ONLY POSITIVE INTEGERS
 // [ 23, 1, 263, 40 ]		=>   [ 1, 23, 40 263 ]
 function radixSort(arr) {
-
 	if (!Array.isArray(arr)) return null;																					// exit if input is not an array
-	if (arr.length <=1) return arr;																								// trivial case if array empty or only has 1 integer
+	if (arr.length <= 1) return arr;																							// trivial case if array empty or only has 1 integer
 
-	// 1) get the max num of digits from all the numbers in the input array
-	let maxDigits = getMaxDigits(arr);
-	//  maxDigits = 3
+	let L = getMaxDigits(arr);																										// 1) Find L (length of longest integer), O(N) Time
 
-	// 2) Loop number of times equal to maxDigits
-	for (let k = 1; k <= maxDigits; k++) {
+	for (let i = 1; i <= L; i++) {																								// 2) loop L times
+		let buckets = [[], [], [], [], [], [], [], [], [], []];											// 3) create "bucket" (array) of 10 sub arrays. using base 10 radix so each bucket refers to digit 0-9
 
-		// 3) create 10 buckets (bec we're using base 10 radix, each bucket each representing digits 0-9)
-		let buckets = [ [], [], [], [], [], [], [], [], [], [] ];
-		
-		// 4) loop through integers, looking at only the digit in the kth place
-		for (let i = 0; i < arr.length; i++) {
-	
-			let num = arr[i];
-			// i = 0:	num = 23
-			// i = 1:	num = 1
-			// i = 2:	num = 263
-			// i = 3:	num = 40
-	
-			// 4) Get digit from the kth place
-			let digit = getDigitFrom(num, k);
-			// i = 0: digit = 3
-			// i = 1: digit = 1
-			// i = 2: digit = 3
-			// i = 3: digit = 0
-	
-			// 5) Add integer to appropriate bucket, (determined by digit)
-			buckets[digit].push(num);
-			// i = 0: buckets = [ [], [], [], [23], [], [], [], [], [], [] ]
-			// i = 1: buckets = [ [], [1], [], [23], [], [], [], [], [], [] ]
-			// i = 2: buckets = [ [], [1], [], [23, 263], [], [], [], [], [], [] ]
-			// i = 3: buckets = [ [40], [1], [], [23, 263], [], [], [], [], [], [] ]
+		for (let j = 0; j < arr.length; j++) {																			// 4) loop through array
+			let num = arr[j];
+			let lthDigit = getDigit(num, i);																					// 5) grab digit in ith place, O(1) Time
+			buckets[lthDigit].push(num);																							// 6) add integer in appropriate bucket (determined by lth digit)
 		}
-	
-		// 6) Create new list by "flattening" numbers in buckets
-		arr = [].concat(...buckets);
-		// arr = [ 40, 1, 23, 263 ]
+
+		arr = [].concat(...buckets);																								// 7) "flatten" nums in bucket into one array of nums, O(N) Time
 	}
 
 	return arr;
 }
-// console.log(radixSort([23, 1, 263, 40]));																				//=> [ 1, 23, 40 263 ]
+// console.log(radixSort([23, 1, 263, 40]));																		//=> [ 1, 23, 40 263 ]
 
 
 
-// Helper function
-// (23, 1)			=>   3
-// (23, 2)			=>   2
-function getDigitFrom(num, nthPlace) {																					// nthPlace = nth place from right of num (not same as place value in ones, tens, etc...)
-	// return Math.floor(Math.abs(num) / Math.pow(10, nthPlace - 1)) % 10;				// for negative nums
-	return Math.floor(num/Math.pow(10, nthPlace - 1)) % 10;
-}
-// console.log(getDigitFrom(23, 1));					//=> 3
-// console.log(getDigitFrom(23, 2));					//=> 2
-
-
-
-// Helper function
+// Helper function, O(1) Time
 // 23 	=>	2
 // 234 	=>	3
 function getIntLength(num) {
@@ -78,13 +41,10 @@ function getIntLength(num) {
 		return Math.floor(Math.log10(Math.abs(num))) + 1;
 	}
 }
-// console.log(getIntLength(23));								//=> 2
-// console.log(getIntLength(234));							//=> 3
 
 
-
-// Helper Function
-// [ 23, 1, 269, 40, 0 ]			=>    3						bec 269 has the most digits and has 3 digits
+// Helper function, O(1) Time
+// [ 23, 1, 269, 40, 0 ]			=>    3																						// bec 269 has the most digits (3 digits)
 function getMaxDigits(nums) {
 	let maxDigits = 0;
 	for (let i = 0; i < nums.length; i++) {
@@ -92,22 +52,48 @@ function getMaxDigits(nums) {
 	}
 	return maxDigits;
 }
-// console.log(getMaxDigits([ 23, 1, 269, 40, 0 ]));														//=> 3
+
+
+// Helper Function, O(1) Time
+// (23, 1)			=>   3
+// (23, 2)			=>   2
+function getDigit(number, i) {
+	return Math.floor(number / Math.pow(10, i - 1)) % 10;
+}
 
 
 
 
-// 2.3		=>  2
-// function myFloor(num) {
-// 	let remainder = num % 1;
-
-// 	return num - remainder;
-// }
-// console.log(myFloor(2.3));		//=> 2
-// console.log(myFloor(2.6));		//=> 2
-// console.log(myFloor(.6));			//=> 0
 
 
+// *****************************************************************************
+// SOLUTION V2- WORKS FOR ALL INTEGERS POSTIVE/NEGATIVE
+// TIME COMPLEXITY: 		O(N * L),		N = array length, L = length of longest integer (maxDigits)
+// SPACE COMPLEXITY: 		O(N + L)
+// - Uses helper function radixSort to sort negatives
+// - concats sorted negatives and sorted positives
+// - uses all functions above in V1
+// Runtime: 74% faster than JS submissions
+// Memory: 31% less than JS submissions
+// DOES NOT Mutate the original array
+// [23, -1, 263, -40]			=>    [ -40, -1, 23, 263 ]
+function radixSortWithNegatives(arr) {
+	if (!Array.isArray(arr)) return null;																					// exit if input is not an array
+	let negatives = arr.filter(integer => integer < 0);														// 1) filter negative integers
+
+	if (negatives.length > 0) {																										// 2) if there are negatives, sort them using radixSort
+		negatives = radixSort(negatives.map(num => Math.abs(num)))									// remove negative signs, + O(N)Time, then radixSort
+			.reverse()																																// reverse nums,				  + O(N)Time
+			.map(num => -num);																												// add negative signs,		+ O(N)Time
+	}
+
+	let positives = arr.filter(integer=> integer >= 0);														// 3) filter positive integers, O(N) Time
+
+	if (positives.length > 0) positives = radixSort(positives);										// 4) if there are positives, sort them using radixSort
+
+	return negatives.concat(positives);																						// 5) concat negatives with positives
+}
+// console.log(radixSortWithNegatives([23, -1, 263, -40]));											//=> [ -40, -1, 23, 263 ]
 
 
 module.exports = {
