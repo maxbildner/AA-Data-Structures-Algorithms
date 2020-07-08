6010bbace79a2c2865e5b1719330381683a333fb
mbildner <mbildner@icloud.com>
2020-07-08 12:16:55 -0400

    determine if binary tree is balanced using top down recursion

--- 11-Binary-Search-Trees/bst_project/lib/leet_code_108.js
+++ 11-Binary-Search-Trees/bst_project/lib/leet_code_108.js
@@ -1,7 +1,35 @@
-// View the full problem and run the test cases at:
-//  https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+// LEETCODE LC 108 Convert Sorted Array to Binary Search Tree
+// EASY
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+// Given an array where elements are sorted in ascending order, convert it to a 
+// height balanced BST. For this problem, a height - balanced binary tree is 
+// defined as a binary tree in which the depth of the two subtrees of every node
+// never differ by more than 1.
+// INPUT:  Array of node values (integers)
+// OUTPUT: Tree Node (representing root)
+// 
+// Example:
+// [-10, -3, 0, 5, 9]
+// One possible answer is: [0, -3, 9, -10, null, 5], which represents the 
+// following height balanced BST:
+//       0
+//      / \
+//    -3   9
+//    /   /
+// - 10  5
+// 
+// class TreeNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
 
+// VERSION 1- 
+// TIME COMPLEXITY: 
+// SPACE COMPLEXITY: 
 function sortedArrayToBST(nums) {
-
+  
 }
--- 11-Binary-Search-Trees/bst_project/lib/leet_code_110.js
+++ 11-Binary-Search-Trees/bst_project/lib/leet_code_110.js
@@ -1,7 +1,71 @@
-// View the full problem and run the test cases at:
-//  https://leetcode.com/problems/balanced-binary-tree/
+// LEETCODE LC 110 Balanced Binary Tree
+// EASY
+// https://leetcode.com/problems/balanced-binary-tree/
+// Given a binary tree, determine if it is height - balanced.
+// For this problem, a height - balanced binary tree is defined as:
+// a binary tree in which the left and right subtrees of every node differ in 
+// height by no more than 1.
+// INPUT:  Tree Node (representing root)
+// OUTPUT: Boolean
+// 
+// Example 1:
+// Given the following tree [3, 9, 20, null, null, 15, 7]:
+//       3
+//      / \
+//     9  20
+//       /  \
+//      15   7
+// Return true
+// 
+// Example 2:
+// Given the following tree [1, 2, 2, 3, 3, null, null, 4, 4]:
+//          1
+//         / \
+//        2   2
+//       / \
+//      3   3
+//     / \
+//    4   4
+// Return false
+// 
+// Definition of Tree Node:
+// class TreeNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
 
 
+// *****************************************************************************
+// VERSION 1- AA SOLUTION, TOP DOWN RECURSIVE
+// TIME COMPLEXITY:  O(N log(N)),    N = number of nodes in tree
+//      N first N from = ?  helper function getHeight that's called d times (depth)
+//      log(N) comes from = ?
+// SPACE COMPLEXITY: O(N)            recursion stack may contain all nodes if tree is skewed/not balanced
+// (Tree Node) => boolean 
 function isBalanced(root) {
+  if (!root) return true;                                                       // 1) base case if no tree/root, tree is balanced
 
+  let heightDifference = getHeight(root.left) - getHeight(root.right);          // 2) find height difference of left and right subtrees
+  
+  let heightIsBalanced = Math.abs(heightDifference) <= 1;                       // 3) check if height difference is balanced
+
+  return heightIsBalanced && isBalanced(root.left) && isBalanced(root.right);   // 4) recursively check all subtrees for balanced
 }
+
+// Helper function- Recursive
+// TIME COMPLEXITY:   O(N),       N = number of nodes in tree
+// SPACE COMPLEXITY:  O(N)        (worst case, tree completely unbalanced, and all nodes have to be visited/stack called)
+// (root node) => height integer
+function getHeight(root) {
+  if (!root) return -1;                                                         // 1) base case if there's no root, height = -1
+  return Math.max(getHeight(root.left), getHeight(root.right)) + 1;             // 2) recursive call. "+ 1" is the height of the current root nodes height
+}
+
+
+
+
+// *****************************************************************************
+// VERSION 2- LC SOLUTION, RECURSIVE
