e8ad7348a46135293d7fb2d57a8ec5a4e4536587
mbildner <mbildner@icloud.com>
2020-07-09 15:11:42 -0400

    upload graph traversal notes

--- 12-Graphs/scratch.js
+++ 12-Graphs/scratch.js
@@ -11,25 +11,25 @@
 
 // *****************************************************************************
 // 1) Clunky Graph Implementation
-class GraphNode {
-  constructor(val) {
-    this.val = val;
-    this.neighbors = [];                                                        // contains references to other nodes
-  }
-}
+// class GraphNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.neighbors = [];                                                        // contains references to other nodes
+//   }
+// }
 
 
 // Example 1:
-let a = new GraphNode('a');
-let b = new GraphNode('b');
-let c = new GraphNode('c');
-let d = new GraphNode('d');
-let e = new GraphNode('e');
-let f = new GraphNode('f');
-a.neighbors = [b, c, e];
-c.neighbors = [b, d];
-e.neighbors = [a];
-f.neighbors = [e];
+// let a = new GraphNode('a');
+// let b = new GraphNode('b');
+// let c = new GraphNode('c');
+// let d = new GraphNode('d');
+// let e = new GraphNode('e');
+// let f = new GraphNode('f');
+// a.neighbors = [b, c, e];
+// c.neighbors = [b, d];
+// e.neighbors = [a];
+// f.neighbors = [e];
 // GRAPH 1:
 //      E  ↔  A → B
 //      ↑     ↓ ↗
@@ -88,3 +88,169 @@ let graph = {
   'f': ['e']
 };
 // - Space = number of edges
+
+
+// *****************************************************************************
+// 4) Graph Traversals- DEPTH FIRST using Nodes and neighbors array
+//      E  ↔  A → B
+//      ↑     ↓ ↗
+//      F     C
+//            ↓
+//            D
+// - we'll make F our starting "root" since it can access other nodes
+// - DF =  F  E  A  B  C  D
+
+class GraphNode {
+  constructor(val) {
+    this.val = val;
+    this.neighbors = [];                                                        // contains references to other nodes
+  }
+}
+
+let a = new GraphNode('a');
+let b = new GraphNode('b');
+let c = new GraphNode('c');
+let d = new GraphNode('d');
+let e = new GraphNode('e');
+let f = new GraphNode('f');
+a.neighbors = [b, c, e];
+c.neighbors = [b, d];
+e.neighbors = [a];
+f.neighbors = [e];
+
+// VERSION 1- RECURSIVE, Only works if node values are unique!
+function depthFirstRecur(node, visited=new Set()) {
+  if (visited.has(node.val)) return;                                            // exit if node has already been visited
+
+  console.log(node.val);
+  visited.add(node.val);                                                        // add node val to set
+
+  node.neighbors.forEach(neighbor => {
+    depthFirstRecur(neighbor, visited);
+  });
+}
+
+// depthFirstRecur(f);   //=> 
+// 'f'
+// 'e'
+// 'a'
+// 'b'
+// 'c'
+// 'd'
+
+
+// VERSION 2- ITERATIVE, Only works if node values are unique!
+function depthFirstIter(node) {
+  let visited = new Set();
+  let stack = [ node ];
+
+  while (stack.length) {
+    let node = stack.pop();                                                     // remove last node from stack (we're going to "visit" this node)
+
+    if (visited.has(node.val)) continue;                                        // skip this node, if we've visited it already
+
+    console.log(node.val);                                                      // print out node val, and mark as visited
+    visited.add(node.val);
+
+    stack.push(...node.neighbors);                                              // add nodes neighbors to stack to be explored
+  }
+}
+
+// depthFirstIter(f);   //=> 
+// 'f'
+// 'e'
+// 'a'
+// 'b'
+// 'c'
+// 'd'
+
+
+
+
+// *****************************************************************************
+// 5) Graph Traversals- DEPTH FIRST using Adjacency List
+//      E  ↔  A → B
+//      ↑     ↓ ↗
+//      F     C
+//            ↓
+//            D
+// - we'll make F our starting "root" since it can access other nodes
+// - DF =  F  E  A  B  C  D
+let graph2 = {
+  'a': ['b', 'c', 'e'],
+  'b': [],
+  'c': ['b', 'd'],
+  'd': [],
+  'e': ['a'],
+  'f': ['e']
+};
+
+
+// VERSION 3- RECURSIVE, Only works if values are unique! 
+// NOT THE BEST BEC all nodes may not be reached if you pick a poor root/start
+function depthFirstRecurV2(node, graph, visited=new Set()) {
+  if (visited.has(node)) return;                                                // exit if node has already been visited
+
+  console.log(node);
+  visited.add(node);                                                            // add node to set
+
+  graph[node].forEach(neighbor => {
+    depthFirstRecurV2(neighbor, graph, visited);
+  });
+}
+
+// depthFirstRecurV2('f', graph2);   //=> 
+// 'f'
+// 'e'
+// 'a'
+// 'b'
+// 'c'
+// 'd'
+
+
+// VERSION 4- ITERATIVE, Only works if values are unique! 
+// ALL nodes ARE reached even if you pick a poor root/start
+function depthFirst(graph) {
+  let visited = new Set();
+
+  for (let node in graph) {                                                     // loop through all keys (nodes) in graph
+    _depthFirstRecur(node, graph, visited);
+  }
+}
+
+// HELPER
+function _depthFirstRecur(node, graph, visited) {
+  if (visited.has(node)) return;
+
+  console.log(node);
+  visited.add(node);
+
+  graph[node].forEach(neighbor => {
+    _depthFirstRecur(neighbor, graph, visited);
+  });
+}
+
+// depthFirst(graph2);   //=> 
+// 'f'
+// 'e'
+// 'a'
+// 'b'
+// 'c'
+// 'd'
+
+// EXAMPLE 2 (1 graph, but disconnected)
+let graph3 = {
+  'h': ['i', 'j'],
+  'i': [],
+  'j': ['k'],
+  'k': [],
+  'l': ['m'],
+  'm': []
+};
+//      H  →  I   L
+//      ↑         ↓ 
+//      J         M
+//      ↓     
+//      K  
+// depthFirst(graph3);   //=> h, i, j, k, l, m
+// https://open.appacademy.io/learn/self-study/data-structures-and-algorithms/graph-traversal-notes
