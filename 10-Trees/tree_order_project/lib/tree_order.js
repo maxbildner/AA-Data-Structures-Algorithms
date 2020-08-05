dfb764fb929f9a3d90ebc7edcbdf9dff91ce493b
mbildner <mbildner@icloud.com>
2020-08-05 12:52:05 -0400

    write recursive and iterative solutions for in, pre, post order binary tree traversals

--- 10-Trees/tree_order_project/lib/tree_order.js
+++ 10-Trees/tree_order_project/lib/tree_order.js
@@ -28,58 +28,106 @@ c.right = f;
 
 
 // *****************************************************************************
-// IN ORDER-			   LEFT, SELF, RIGHT
-// [TreeNode A] => [ 'd', 'b', 'e', 'a', 'c', 'f' ]
-function inOrderArray(root) {
-	if (root === null) return [];				// base case, if no node given
-	// 1: root = A   false
-	// 2: root = B	 false
-	// 3: root = D	 false
-
+// PRE ORDER RECURSIVE-  S, L, R
+// (TreeNode)   => 	Array
+// (TreeNode A)	=> 	[ A, B, D, E, C, F ]
+function preOrderArray(root) {
 	let path = [];
-
-	path = inOrderArray(root.left);
-	// 1: inOrderArray(B)			=> 					 path = [ 'd', 'b', 'e' ]
-	// 2: inOrderArray(D)			=> [ 'd' ]   path = [ 'd' ]
-	// 3: inOrderArray(null)	=> []
+	if (!root) return [];
 
 	path.push(root.val);
-	// 3: [].push(D.val)											path = [ 'd' ]
-	// 2: [ 'd' ].push(B.val)									path = [ 'd', 'b' ]
-	// 1: [ [ 'd', 'b', 'e' ] ].push(A.val)		path = [ 'd', 'b', 'e', 'a' ]
+	path = path.concat(preOrderArray(root.left));
+	path = path.concat(preOrderArray(root.right));
 
-	path = path.concat(inOrderArray(root.right));
-	// 3: inOrderArray(null)  => []
-	// 2: path = [ 'd', 'b' ].concat(inOrderArray(E)) = [ 'd', 'b' ].concat(['e]
-	// 2: path = [ 'd', 'b', 'e' ]
-	// 1: path = [ 'd', 'b', 'e', 'a' ].concat(inOrderArray(C))
- 
 	return path;
-	// 3: [ 'd' ]
 }
-// console.log(inOrderArray(a));		=>   [ 'd', 'b', 'e', 'a', 'c', 'f' ]
 
+
+// PRE ORDER ITERATIVE
+// (TreeNode A) => [ A, B, D, E, C, F ]
+// (TreeNode B)	=> [ B, D, E ]
+function preOrderArrayIter(root) {
+	let path = [];
+	let stack = [root];
+
+	while (stack.length) {																												// loop while stack is NOT empty
+		let node = stack.pop(root);																									// "visit" root node/top of stack
+		path.push(node.val);																												// push current node val to path
+
+		if (node.right) stack.push(node.right);																			// add right/left nodes of current node to stack
+		if (node.left) stack.push(node.left);
+	}
+
+	return path;
+}
+
+
+// *****************************************************************************
+// IN ORDER RECURSIVE - L, S, R
+// (TreeNode)   => 	Array
+// (TreeNode A)	=> 	[ D, B, E, A, C, F ]
+// (TreeNode B)	=> 	[ D, B, E ]
+// (TreeNode D)	=> 	[ D ]
+function inOrderArray(root) {								// L, S, R
+	let path = [];
+	if (!root) return [];
+	path = inOrderArray(root.left);
+	path.push(root.val);
+	path = path.concat(inOrderArray(root.right));
+	return path;
+}
+
+
+// IN ORDER ITERATIVE 
+// (TreeNode)   => 	Array
+// (TreeNode A)	=> 	[ D, B, E, A, C, F ]
+// (TreeNode B)	=> 	[ D, B, E ]
+// (TreeNode D)	=> 	[ D ]
+function inOrderArrayIter(root) {
+	let path = [];
+	let stack = [];
+	let node = root;
+
+	while (stack.length || node) {
+		if (node) {
+			stack.push(node);
+			node = node.left;
+
+		} else {
+			node = stack.pop();
+			path.push(node.val);
+			node = node.right;
+		}
+	}
+
+	return path;
+}
 
 
 
 // *****************************************************************************
-// POST ORDER-			  LEFT, RIGHT, SELF
-// [TreeNode A] => [ 'd', 'e', 'b', 'f', 'c', 'a' ]
+// POSTORDER RECURSIVE
+// (TreeNode)   => 	Array
+// (TreeNode A)	=> 	[ D, E, B, F, C, A ]
 function postOrderArray(root) {
-	if (root === null) return [];
-
 	let path = [];
-
-	path = postOrderArray(root.left);
+	if (!root) return [];
+	path = path.concat(postOrderArray(root.left));
 	path = path.concat(postOrderArray(root.right));
 	path.push(root.val);
-
 	return path;
 }
-console.log(postOrderArray(a));		//=> [ 'd', 'e', 'b', 'f', 'c', 'a' ]
 
 
 module.exports = {
 	inOrderArray,
 	postOrderArray
 };
+
+
+// console.log(preOrderArray(root));				//=> [ A, B, D, E, C, F ]
+// console.log(inOrderArray(root));					//=> [ D, B, E, A, C, F ]
+// console.log(postOrderArray(root));				//=> [ D, E, B, F, C, A ]
+
+// console.log(preOrderArrayIter(root));		//=> [ A, B, D, E, C, F ]
+// console.log(inOrderArrayIter(root));			//=> [ D, B, E, A, C, F ]
