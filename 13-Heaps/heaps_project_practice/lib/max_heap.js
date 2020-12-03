839f59d43973f7a5770407689724a2a0c5622eff
mbildner <mbildner@icloud.com>
2020-12-03 12:46:26 -0500

    build maxheap datastructure

--- 13-Heaps/heaps_project_practice/lib/max_heap.js
+++ 13-Heaps/heaps_project_practice/lib/max_heap.js
@@ -16,8 +16,88 @@
 
 // TIME: 30MIN (11/11/20)
 // TIME: 45MIN (11/19/20)
+// TIME: 33MIN (12/3/20)
 class MaxHeap {
+	constructor(){
+		this.array = [ null ];
+	}
+
+
+	getParent(idx) {
+		return Math.floor(idx/2);
+	}
+
+
+	getLeftChild(idx) {
+		return idx * 2;
+	}
+
+
+	getRightChild(idx) {
+		return idx * 2 + 1;
+	}
+
 	
+	insert(val) {
+		this.array.push(val);
+
+		this.siftUp(this.array.length - 1);
+	}
+
+
+	siftUp(idx) {
+
+		if (idx === 1) return;
+
+		let parentIdx = this.getParent(idx);
+		let parentVal = this.array[parentIdx];
+
+		// if current node val > parent node val, max heap property broken, so swap
+		if (this.array[idx] > parentVal) {
+			[ this.array[idx], this.array[parentIdx] ] = 
+			[ this.array[parentIdx], this.array[idx] ];
+
+			this.siftUp(parentIdx);
+		}
+	}
+
+
+	deleteMax() {
+
+		if (this.array.length === 1) return null;
+		if (this.array.length === 2) return this.array.pop();
+
+		let max = this.array[1];
+
+		this.array[1] = this.array.pop();
+
+		this.siftDown(1);
+
+		return max;
+	}
+
+
+	siftDown(idx) {
+		let arr = this.array;
+		let leftChildIdx = this.getLeftChild(idx);
+		let rightChildIdx = this.getRightChild(idx);
+		let leftChildVal = arr[leftChildIdx] || -Infinity;
+		let rightChildVal = arr[rightChildIdx] || -Infinity;
+
+		// exit if current val >= both children
+		if (arr[idx] >= leftChildVal && arr[idx] >= rightChildVal) return;
+
+		// swap current node/val w/ larger of the two children
+		let swapIdx;
+		if (leftChildVal > rightChildVal) {
+			swapIdx = leftChildIdx;
+		} else {
+			swapIdx = rightChildIdx;
+		}
+		[ arr[idx], arr[swapIdx] ] = [ arr[swapIdx], arr[idx] ];
+
+		this.siftDown(swapIdx);
+	}
 }
 
 
